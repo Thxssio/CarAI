3ca708fd489da45cea75643cf7e9e672585a9875
Thassio <thxssio@gmail.com>
2023-11-11 01:14:32 -0300

    mask

--- camera.py
+++ camera.py
@@ -10,11 +10,49 @@ cap = cv.VideoCapture(0)
 prev_frame_time = 0
 new_frame_time = 0
 
+def fps(prev_frame_time, new_frame_time):
+    
+    width = frame.shape[1]
+    height = frame.shape[0]
+
+
+    print(width)
+    print(height)
+
+    new_frame_time = time.time()
+    fps = 1/(new_frame_time-prev_frame_time)
+    prev_frame_time = new_frame_time 
+    fps = int(fps)
+    fps = str(fps)
+    font = cv.FONT_HERSHEY_SIMPLEX 
+    cv.putText(frame, fps, (7, 70), font, 3, (100, 255, 0), 3, cv.LINE_AA)
+    return frame
+
+def circulo(frame, center_coordinates, radius, colorCircle, thicknessCircle):
+    """    
+    center_coordinates = (320, 240)
+    radius = 20
+    colorCircle = (255, 0, 0)
+    thicknessCircle = 2 
+    """
+    frame = cv.circle(frame,  center_coordinates, radius, colorCircle, thicknessCircle)
+    return frame
+
+def linha(frame , start_point, end_point, color, thickness):
+    """   
+    start_point = (0, 240) 
+    end_point = (640, 240)
+    color = (0, 255, 0)
+    thickness = 3
+    """
+    frame = cv.line(frame, start_point, end_point, color, thickness)
+    return frame
 
 
 if not cap.isOpened():
     print("Não foi possivel abrir a camera.")
     exit()
+
 while True:
 
     ret, frame = cap.read()
@@ -24,38 +62,18 @@ while True:
         print("Não foi possivel abrir a stream. Saindo...")
         break
     #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    width = frame.shape[1]
-    height = frame.shape[0]
+    filter = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    lower_green = np.array([36,100,100])
+    upper_green = np.array([80,255,255])
+    mask = cv.inRange(filter, lower_green, upper_green)
 
-    print(width)
-    print(height)
-
-    start_point = (0, 240) 
-    end_point = (640, 240)
-    color = (0, 255, 0)
-    thickness = 3
-
-    center_coordinates = (320, 240)
-    radius = 20
-    colorCircle = (255, 0, 0)
-    thicknessCircle = 2 
+    movimentarNaLinha = 10
+    linha(mask, (0, 240), (640, 240), (0,255,0), 3)
+    circulo(mask, (movimentarNaLinha, 240), 20, (255, 0, 0), 2)
+    fps(prev_frame_time, new_frame_time)
 
 
-    cam = cv.line(frame, start_point, end_point, color, thickness) 
-    cam = cv.circle(frame,  center_coordinates, radius, colorCircle, thicknessCircle)
-
-    
-
-
-
-    new_frame_time = time.time()
-    fps = 1/(new_frame_time-prev_frame_time)
-    prev_frame_time = new_frame_time 
-    fps = int(fps)
-    fps = str(fps)
-    font = cv.FONT_HERSHEY_SIMPLEX 
-    cv.putText(frame, fps, (7, 70), font, 3, (100, 255, 0), 3, cv.LINE_AA)
-    cv.imshow('frame', cam)
+    cv.imshow('frame', mask)
 
 
     if cv.waitKey(1) == ord('q'):
